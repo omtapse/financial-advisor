cfec8c0d44813c570558067676df302569be6511
Anirudh Nandy <shivammm.hyphen@gmail.com>
2024-05-08 01:37:14 +0530

    Added Sidebar

--- src/app/components/AITextBox.tsx
+++ src/app/components/AITextBox.tsx
@@ -7,10 +7,10 @@ type Props = {}
 
 const AITextBox = (props: Props) => {
   return (
-      <div className="flex flex-col-reverse px-4 py-5 h-screen max-w-[790px] relative items-center gap-5">
+      <div className="flex flex-col-reverse px-4 py-5 h-screen max-w-[790px] relative items-center jus gap-5">
 
           <div className="flex flex-row max-w-[790px] gap-2 items-center border-2 border-black rounded-lg">
-              <input placeholder="Hey Shivam, Here to help for your financials" autoFocus={false}
+              <input placeholder="Hey Shivam, Ask me anything..." autoFocus={false}
                      className="bg-white p-4 mx-2 min-w-[700px] rounded-lg outline-none text-black placeholder:text-gray-500"/>
               <button>
                   <Forward className="p-2 m-2 cursor-pointer" color="#226ce0" size={40}/>
--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -7,7 +7,7 @@ function SideBar({}: Props) {
   return (
     <div className='flex flex-col'>
         
-        <div className='flex flex-col h-screen min-w-[230px] rounded-r-xl p-0 gap-4'>
+        <div className='flex flex-col h-screen min-w-[230px] rounded-r-xl p-0 gap-4 justify-stretch'>
             
             <div className='max-h-[100] max-w-[100] px-7 py-5'>
             <Image src={Logo} alt='Logo' width={80} height={100}></Image>
@@ -25,7 +25,7 @@ function SideBar({}: Props) {
             <button className='flex px-8 py-3 self-start hover:bg-gray-400 rounded-lg min-w-[250px] min-h-[40px] text-gray-600 hover:text-white'>Sign In</button>
 
 
-            <button className='flex text-sm justify-center p-4 rounded-lg hover:bg-blue-400 min-w-[10px] min-h-[40px] text-gray-600 hover:text-white'>Sign Up In April</button>
+            <button className='flex text-md justify-center p-4 rounded-r-lg hover:bg-[#226ce0] min-w-[10px] min-h-[40px] text-gray-600 hover:text-white'>Sign Up In April</button>
         </div>      
   
 
