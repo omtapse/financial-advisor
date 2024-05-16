b88c70c64b2834dac84e9245355f87856964c367
Anirudh Nandy <shivammm.hyphen@gmail.com>
2024-05-16 22:19:15 +0530

    Added MutualFunds & Retirement Funds

--- src/app/components/RetirementsFund.tsx
+++ src/app/components/RetirementsFund.tsx
@@ -3,39 +3,28 @@ import React from 'react'
 
 type Props = {}
 
-const MutualFunds = (props: Props) => {
+const RetirementsFund = (props: Props) => {
   return (
     <div className='flex flex-row border-white gap-3'>
 
         <div className='min-w-[70px] p-5 min-h-[20px] flex flex-row items-center gap-3 bg-[#1E1F20] rounded-lg'>
-            <Image src="/assets/quant.png" alt='quant' width={50} height={50}/>
-            <div className='flex flex-col gap-2 items-center'>
-              <p className='text-white'>Quant Small Cap Fund</p>
+            <Image src="/assets/hdfc.png" alt='quant' width={50} height={50}/>
+            <div className='flex flex-col gap-2'>
+              <p className='text-white'>HDFC Retirement Savings Fund - Equity Plan</p>
               <div className='w-fit h-fit flex'>
                 <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md'>Equity</p>
-                <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md'>Small Cap</p>
+                <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md'>Retirement Solutions</p>
             </div>
         </div>
         </div>
 
         <div className='min-w-[70px] p-5 min-h-[20px] flex flex-row items-center gap-3 bg-[#1E1F20] rounded-lg'>
-            <Image src="/assets/quant.png" alt='quant' width={50} height={50}/>
+            <Image src="/assets/hdfc.png" alt='quant' width={50} height={50}/>
             <div className='flex flex-col gap-2'>
-            <p className='text-white'>Quant Mid Cap Fund</p>
+            <p className='text-white'>HDFC Retirement Savings Fund - Hybrid Equity Plan</p>
             <div className='w-fit h-fit flex'>
               <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md'>Equity</p>
-              <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md' >Mid Cap</p>
-            </div>
-            </div>
-        </div>
-
-        <div className='min-w-[70px] break-words p-5 min-h-[20px] flex flex-row items-center gap-3 bg-[#1E1F20] rounded-lg'>
-            <Image src="/assets/kotak.png" alt='quant' width={50} height={50}/>
-            <div className='flex flex-col gap-2'>
-            <p className='text-white'>Kotak Infrastructure and Economic Reform Fund</p>
-            <div className='w-fit h-fit flex'>
-              <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md'>Equity</p>
-              <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md' >Sectorial Infrastructure</p>
+              <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md' >Retirement Solutions</p>
             </div>
             </div>
         </div>
@@ -43,10 +32,21 @@ const MutualFunds = (props: Props) => {
         <div className='min-w-[70px] break-words p-5 min-h-[20px] flex flex-row items-center gap-3 bg-[#1E1F20] rounded-lg'>
             <Image src="/assets/icici.png" alt='quant' width={50} height={50}/>
             <div className='flex flex-col gap-2'>
-            <p className='text-white'>ICICI Prudential Value Discovery Fund</p>
+            <p className='text-white'>ICICI Prudential Retirement Fund - Pure Equity Plan</p>
             <div className='w-fit h-fit flex'>
               <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md'>Equity</p>
-              <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md' >Value Orientated</p>
+              <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md' >Retirement Solutions</p>
+            </div>
+            </div>
+        </div>
+
+        <div className='min-w-[70px] break-words p-5 min-h-[20px] flex flex-row items-center gap-3 bg-[#1E1F20] rounded-lg'>
+            <Image src="/assets/tata.png" alt='quant' width={50} height={50}/>
+            <div className='flex flex-col gap-2'>
+            <p className='text-white'>Tata Retirement Savings Progressive Plan</p>
+            <div className='w-fit h-fit flex'>
+              <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md'>Equity</p>
+              <p className='text-sm mx-2 bg-green-300 text-green-900 px-2 rounded-md' >Retirement Solutions</p>
             </div>
             </div>
         </div>
@@ -55,4 +55,4 @@ const MutualFunds = (props: Props) => {
   )
 }
 
-export default MutualFunds
+export default RetirementsFund
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import AITextBox from "@/app/components/AITextBox";
+import MutualFunds from "./components/MutualFunds";
+import RetirementsFund from "./components/RetirementsFund";
 
 
 
 export default function Home() {
   return (
-    <main className="bg-[#131314] h-screen  flex flex-col items-center" >
-      <AITextBox />
+    <main className="bg-[#131314] h-screen flex flex-col items-center gap-3 justify-center" >
+      {/* <AITextBox /> */}
+{/* 
+      <MutualFunds/>
+      <RetirementsFund/> */}
+
+
     </main>
   );
 }
