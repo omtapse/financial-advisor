52f10d6c22c0c2d970828f87078d96e9c9d101f1
ruturaj <rajghodekar.rg@gmail.com>
2024-05-16 17:44:54 +0530

    props

--- src/app/store/chat.store.tsx
+++ src/app/store/chat.store.tsx
@@ -1,19 +1,27 @@
 import { create } from "zustand";
 
+export enum AppStates {
+    MAIN = 'main',
+    EXTRA = 'extra',
+    FUTURE = 'future'
+}
 interface appStore {
-
+    infoState: any,
+    setInfoState: (state: AppStates) => void
     istemplateOpen: boolean
     toggleTemplate: () => void
     chat: Array<any> | []
     currentChat: any | null
 
-    pushChat: (chat: any) => void;
-
+    pushChat: (chat: any) => void
 }
 
 export const useAppStore = create<appStore>((set, get) => ({
     istemplateOpen: false,
-
+    infoState: null,
+    setInfoState(state) {
+        set({ infoState: state })
+    },
     toggleTemplate() {
         console.log(get().istemplateOpen)
         set({ istemplateOpen: !get().istemplateOpen })
