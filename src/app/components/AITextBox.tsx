271e54b05c4e511e803730d63950775dbb65fdcf
ruturaj <rajghodekar.rg@gmail.com>
2024-05-16 18:53:17 +0530

    updates before

--- src/app/components/AITextBox.tsx
+++ src/app/components/AITextBox.tsx
@@ -1,40 +1,64 @@
 'use client'
 import DidYouKnow from "@/app/components/DidYouKnow";
 import TextHello from "@/app/components/TextHello";
-import { Forward } from "lucide-react";
-import { useAppStore } from '../store/chat.store';
-import { useRef } from "react";
-import { POST } from "../config/openAIConfig";
-import { generateText } from 'ai';
-import { Configuration, OpenAIApi } from 'openai-edge'
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from 'ai';
+import { Forward } from "lucide-react";
+import { useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
+import { useAppStore } from '../store/chat.store';
+import { uuid } from "uuidv4";
+import Markdown from "react-markdown";
 
+function ChatItem({ chat }: any) {
+
+  return <div className="space-y-4 p-6 max-w-xl font-light">
+    <div className="flex ">
+      <span>you : </span>
+      <p>  {chat.question}</p>
+    </div>
+    <div className="flex">
+      <span className="w-max">advisor : </span>
+      <Markdown >{chat.answer}</Markdown>
+    </div>
+  </div>
+}
 
 
 type Props = {}
 
 function ChatBox() {
-  const scroller = useRef(null)
+  const { ref, inView, entry } = useInView();
+  const { chat, currentChat } = useAppStore()
 
-  return <div>
-    <div className="self-start">
+  useEffect(() => {
+    if (!inView) {
+      entry?.target.scrollTo({ behavior: 'smooth' })
+    }
+
+  }, [inView])
+
+  return <div className="h-screen max-w-3xl overflow-auto no-bar">
+    <div className="  no-bar self-start">
       <DidYouKnow />
     </div>
 
     <div className="self-start">
       <TextHello />
     </div>
-    <div ref={scroller}></div>
+    <div className="text-white font-sans text-justify bg-[#1E1F20] rounded-xl w-full mb-20 ">
+      {Array.isArray(chat) && chat.map((item) => <ChatItem key={item._id} chat={item} />)}
+      {currentChat && <ChatItem ref={ref} key={currentChat._id} chat={currentChat} />}
+    </div>
   </div>
 }
 
 const AITextBox = (props: Props) => {
   const { istemplateOpen } = useAppStore()
   return (
-    <div className="flex flex-col overflow-auto px-4 py-5 h-screen min-w-4xl relative items-center  no-bar gap-5 ">
+    <div className="flex flex-col overflow-auto px-4 py-8 h-screen min-w-4xl relative items-center  no-bar gap-5 ">
 
-      <div className={`  transition-all  duration-300 ${istemplateOpen ? 'h-[0vh]' : 'h-[30vh]'} `}></div>
+      <div className={`transition-all min-w-3xl max-w-3xl  duration-300 ${istemplateOpen ? 'h-[0vh]' : 'h-[30vh]'} `}></div>
       <ChatBox />
       <SearchComponent />
 
@@ -48,7 +72,7 @@ export default AITextBox
 
 
 function SearchComponent() {
-  const { istemplateOpen, chat, pushChat } = useAppStore();
+  const { istemplateOpen, chat, setCurrentChat } = useAppStore();
   const inputRef = useRef<HTMLInputElement>(null);
 
   const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GOOGLE_API_KEY || '');
@@ -78,11 +102,18 @@ function SearchComponent() {
   }
 
   const handleSubmitQuery = async () => {
+    const query = inputRef.current?.value
+    if (!query) return
+    inputRef.current.value = '';
+    inputRef.current.focus();
+
+    const chat = {
+      _id: uuid(),
+      question: query,
+      answer: ''
+    }
+    setCurrentChat(chat)
     try {
-      const query = inputRef.current?.value
-      if (!query) return
-      inputRef.current.value = '';
-      inputRef.current.focus();
 
       const response = await POST({
         json: async () => ({
@@ -104,12 +135,19 @@ function SearchComponent() {
 
       const decoder = new TextDecoder();
       let done = false;
-
+      let answer = ''
       while (!done) {
         const { value, done: doneReading } = await reader.read();
         done = doneReading;
         const chunkValue = decoder.decode(value, { stream: true });
-        console.log(chunkValue);
+
+        answer = answer + chunkValue.replace('0:', '')
+        const chat = {
+          _id: uuid(),
+          question: query,
+          answer: answer
+        }
+        setCurrentChat(chat)
       }
 
       // const stream = await response.
@@ -118,12 +156,12 @@ function SearchComponent() {
     }
   }
 
-  return <div className={`flex  flex-row max-w-[790px] ${istemplateOpen ? 'absolute bottom-10' : ''} gap-2 items-center  border-2 border-[#333537] rounded-lg bg-[#1E1F20]`}>
+  return <div className={`flex max-w-3xl w-full  flex-row  ${istemplateOpen ? 'absolute bottom-10' : ''} gap-2 items-center  border-2 border-[#333537] rounded-lg bg-[#1E1F20]`}>
     <input placeholder="Hey Shivam, Ask me anything..."
       ref={inputRef}
 
       autoFocus={false}
-      className={`p-4 mx-2 min-w-[700px] rounded-lg outline-none text-[#E0E2E5] placeholder:text-[#E0E2E5] bg-transparent ${!istemplateOpen ? ' animate-pulse' : ''}`} />
+      className={`p-4 mx-2  w-full rounded-lg outline-none text-[#E0E2E5] placeholder:text-[#E0E2E5] bg-transparent ${!istemplateOpen ? ' animate-pulse' : ''}`} />
     <button>
       <Forward onClick={handleSubmitQuery} className="p-2 m-2 cursor-pointer" color="#226ce0" size={40} />
     </button>
--- src/app/store/chat.store.tsx
+++ src/app/store/chat.store.tsx
@@ -12,13 +12,20 @@ interface appStore {
     toggleTemplate: () => void
     chat: Array<any> | []
     currentChat: any | null
-
+    setCurrentChat: (data: any) => void
     pushChat: (chat: any) => void
 }
 
 export const useAppStore = create<appStore>((set, get) => ({
     istemplateOpen: false,
     infoState: null,
+    setCurrentChat(chat: any) {
+        // if (get().currentChat) {
+        //     get().pushChat(get().currentChat)
+        //     set({ currentChat: null })
+        // }
+        set({ currentChat: chat })
+    },
     setInfoState(state) {
         set({ infoState: state })
     },
@@ -26,7 +33,12 @@ export const useAppStore = create<appStore>((set, get) => ({
         console.log(get().istemplateOpen)
         set({ istemplateOpen: !get().istemplateOpen })
     },
-    chat: [],
+    chat: [{ _id: 1, answer: 'answer the wsiaasdfk j', question: 'whati adskfjasldkfa' },
+    { _id: 2, answer: 'answer the wsiaasdfk j', question: 'whati adskfjasldkfa' },
+    { _id: 3, answer: 'answer the wsiaasdfk j', question: 'whati adskfjasldkfa' },
+    { _id: 4, answer: 'answer the wsiaasdfk j', question: 'whati adskfjasldkfa' },
+    { _id: 6, answer: 'answer the wsiaasdfk j', question: 'whati adskfjasldkfa' },
+    ],
     currentChat: null,
     pushChat(chat) {
         set({ chat: [...get().chat, chat] })
